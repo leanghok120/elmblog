f6ad3bafd675ce87ebff4bfa2642cad7f5a55e6f
leanghok120 <leanghokoeng5@gmail.com>
2024-11-26 19:55:43 +0700

    Redesign markdown editor

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,24 +1,27 @@
 import React, { useState } from "react";
-import ReactMarkdown from "react-markdown";
+import {
+  headingsPlugin,
+  listsPlugin,
+  markdownShortcutPlugin,
+  MDXEditor,
+  quotePlugin,
+} from "@mdxeditor/editor";
 
-function Editor({ isPreview }) {
-  const [content, setContent] = useState("");
+function Editor() {
+  const [content, setContent] = useState("# Start writing...");
 
   return (
-    <div className="mt-10">
-      {isPreview ? (
-        <ReactMarkdown className="leading-relaxed text-lg">
-          {content}
-        </ReactMarkdown>
-      ) : (
-        <textarea
-          value={content}
-          onChange={(e) => setContent(e.target.value)}
-          placeholder="Start writing... (Markdown is supported)"
-          className="w-full bg-transparent resize-none outline-none leading-relaxed text-lg min-h-[70vh]"
-        />
-      )}
-    </div>
+    <MDXEditor
+      markdown={content}
+      onChange={(newContent) => setContent(newContent)}
+      plugins={[
+        headingsPlugin(),
+        listsPlugin(),
+        quotePlugin(),
+        markdownShortcutPlugin(),
+      ]}
+      contentEditableClassName="prose outline-none"
+    />
   );
 }
 
--- src/pages/Write.jsx
+++ src/pages/Write.jsx
@@ -1,30 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 import Sidebar from "../components/Sidebar";
 import Editor from "../components/Editor";
-import ToggleEditor from "../components/ToggleEditor";
 
 function Write() {
-  const [title, setTitle] = useState("Title");
-  const [isPreview, setIsPreview] = useState(false);
-
   return (
     <>
       <Sidebar />
       <div className="h-full flex justify-center">
         <div className="p-4 md:p-10 w-full max-w-2xl mx-auto">
-          <ToggleEditor isPreview={isPreview} setIsPreview={setIsPreview} />
-
-          {isPreview ? (
-            <h1 className="text-3xl md:text-4xl font-black">{title}</h1>
-          ) : (
-            <input
-              className="text-3xl md:text-4xl font-black bg-transparent outline-none w-full"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-            />
-          )}
-
-          <Editor isPreview={isPreview} />
+          <Editor />
         </div>
       </div>
     </>
