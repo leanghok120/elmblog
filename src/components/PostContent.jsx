29cc7669004a9bba3644aad84e8523a46c551d18
leanghok120 <leanghokoeng5@gmail.com>
2024-11-28 05:31:08 +0700

    Add info about the post

--- src/components/PostContent.jsx
+++ src/components/PostContent.jsx
@@ -35,9 +35,12 @@ function PostContent() {
           <Loader2 className="animate-spin mt-5" />
         ) : (
           <div className="p-4 md:p-10 w-full max-w-2xl mx-auto">
-            <h1 className="text-3xl md:text-4xl font-black mb-6">
-              {post.title}
-            </h1>
+            <h1 className="text-3xl md:text-4xl font-black">{post.title}</h1>
+            <div className="flex gap-2 text-gruvbox-gray mb-6">
+              <p className="font-bold">{post.author}</p>
+              <span>&middot;</span>
+              <p className="italic">{post.date}</p>
+            </div>
             <MDXEditor
               markdown={post.content}
               readOnly
