a62a1d3d1f4dd64bf42631b2c66724c21d99e121
leanghok120 <leanghokoeng5@gmail.com>
2024-11-27 20:05:47 +0700

    Turn h1 into input

--- src/pages/Write.jsx
+++ src/pages/Write.jsx
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Editor from "../components/Editor";
 import Navbar from "../components/Navbar";
 
 function Write() {
+  const [title, setTitle] = useState("Title");
+
   return (
     <>
       <Navbar />
       <div className="h-full flex justify-center">
         <div className="p-4 md:p-10 w-full max-w-2xl mx-auto">
-          <h1 className="text-3xl md:text-4xl font-black mb-6">Title</h1>
+          <input
+            className="text-3xl md:text-4xl font-black mb-6 bg-transparent outline-none"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
           <Editor />
         </div>
       </div>
