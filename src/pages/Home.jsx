298c08ca03d0bd1aadeac9d14bc905a116ee8e01
leanghok120 <leanghokoeng5@gmail.com>
2024-11-27 19:24:58 +0700

    Setup backend

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Post from "../components/Post";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
+import db from "../appwrite/databases";
 
 function Home() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  async function fetchPosts() {
+    const response = await db.posts.list();
+
+    setPosts(response.documents);
+  }
+
   return (
     <>
       <Navbar />
@@ -11,24 +24,15 @@ function Home() {
         <div className="p-4 md:p-10 w-full max-w-2xl mx-auto">
           <h1 className="text-3xl md:text-4xl font-black">Home</h1>
           <div className="mt-6 md:mt-10 space-y-4 md:space-y-5">
-            <Post
-              id={1}
-              title="My Neovim Setup"
-              date="02, Oct, 2024"
-              author="Leanghok"
-            />
-            <Post
-              id={2}
-              title="Learning Fullstack Development"
-              date="05, Mar, 2023"
-              author="Leanghok"
-            />
-            <Post
-              id={3}
-              title="Morgen Calendar"
-              date="09, Jan, 2021"
-              author="Leanghok"
-            />
+            {posts.map((post) => (
+              <Post
+                key={post.$id}
+                id={post.$id}
+                title={post.title}
+                date={post.date}
+                author={post.author}
+              />
+            ))}
           </div>
           <Footer />
         </div>
