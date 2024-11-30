7331a6da7f86b92270856fb4b1f7c9cfe273ff7c
leanghok120 <leanghokoeng5@gmail.com>
2024-11-30 16:08:45 +0700

    Connect signup/login page to backend

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../components/Navbar";
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
+import { account } from "../appwrite/config";
 
 function Login() {
-  function handleSubmit(e) {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  async function handleSubmit(e) {
     e.preventDefault();
+    setIsLoading(true);
+    try {
+      await account.createEmailPasswordSession(email, password);
+
+      navigate("/");
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setIsLoading(false);
+    }
   }
 
   return (
@@ -16,22 +33,25 @@ function Login() {
           <form className="space-y-3 my-6" onSubmit={handleSubmit}>
             <input
               placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               type="email"
               className="p-3 w-full bg-transparent border-gruvbox-bg2 border rounded-xl outline-none"
             />
             <input
-              placeholder="Username"
-              type="text"
-              className="p-3 w-full bg-transparent border-gruvbox-bg2 border rounded-xl outline-none"
-            />
-            <input
               placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
               type="password"
               className="p-3 w-full bg-transparent border-gruvbox-bg2 border rounded-xl outline-none"
             />
-            <button className="bg-gruvbox-fg text-gruvbox-bg w-full p-3 rounded-xl font-bold transition-all hover:opacity-85 active:opacity-70">
+            <button
+              disabled={isLoading}
+              className="bg-gruvbox-fg text-gruvbox-bg w-full p-3 rounded-xl font-bold transition-all hover:opacity-85 active:opacity-70"
+            >
               Login
             </button>
+            {error && <p className="text-red-500 text-center">{error}</p>}
           </form>
           <Link to="/signup" className="underline">
             Don't have an account?
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../components/Navbar";
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
+import { account } from "../appwrite/config";
 
 function Signup() {
-  function handleSubmit(e) {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  async function handleSubmit(e) {
     e.preventDefault();
+    setIsLoading(true);
+    try {
+      await account.create("unique()", email, password, username);
+      navigate("/");
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setIsLoading(false);
+    }
   }
 
   return (
@@ -16,22 +33,32 @@ function Signup() {
           <form className="space-y-3 my-6" onSubmit={handleSubmit}>
             <input
               placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               type="email"
               className="p-3 w-full bg-transparent border-gruvbox-bg2 border rounded-xl outline-none"
             />
             <input
               placeholder="Username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
               type="text"
               className="p-3 w-full bg-transparent border-gruvbox-bg2 border rounded-xl outline-none"
             />
             <input
               placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
               type="password"
               className="p-3 w-full bg-transparent border-gruvbox-bg2 border rounded-xl outline-none"
             />
-            <button className="bg-gruvbox-fg text-gruvbox-bg w-full p-3 rounded-xl font-bold transition-all hover:opacity-85 active:opacity-70">
+            <button
+              disabled={isLoading}
+              className="bg-gruvbox-fg text-gruvbox-bg w-full p-3 rounded-xl font-bold transition-all hover:opacity-85 active:opacity-70"
+            >
               Signup
             </button>
+            {error && <p className="text-red-500 text-center">{error}</p>}
           </form>
           <Link to="/login" className="underline">
             Already have an account?
