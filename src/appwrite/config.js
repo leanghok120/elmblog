8b2185227fc861c4a4aa248e0673b7ab8c38e77a
leanghok120 <leanghokoeng5@gmail.com>
2024-12-05 19:38:16 +0700

    Remove everything

--- src/appwrite/config.js
+++
@@ -1,9 +0,0 @@
-import { Account, Client, Databases } from "appwrite";
-
-const client = new Client();
-client.setProject(import.meta.env.VITE_PROJECT_ID);
-
-const databases = new Databases(client);
-const account = new Account(client);
-
-export { client, databases, account };
