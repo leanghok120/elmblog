11a064166f9ddb2fb78ff7c2013ed47147d84cd7
leanghok120 <leanghokoeng5@gmail.com>
2024-12-07 15:43:44 +0700

    Setup prisma

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,6 +1,9 @@
 import type { MetaFunction } from "@remix-run/node";
+import type { Post } from "@prisma/client";
 import PostCard from "~/components/PostCard";
-import Footer from "../components/Footer";
+import Footer from "~/components/Footer";
+import prisma from "~/utils/db";
+import { useLoaderData } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -9,17 +12,28 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+export async function loader() {
+  const posts: Post[] = await prisma.post.findMany();
+
+  return posts;
+}
+
 export default function Index() {
+  const posts = useLoaderData<Post[]>();
+
   return (
     <>
       <h1 className="text-3xl md:text-4xl font-black">Home</h1>
       <div className="mt-8 space-y-4">
-        <PostCard
-          id={1}
-          title={"Building elm"}
-          date={"02, 10, 2024"}
-          author={"Leanghok"}
-        />
+        {posts.map((post) => (
+          <PostCard
+            key={post.id}
+            id={post.id}
+            title={post.title}
+            date={post.date}
+            author="Leanghok"
+          />
+        ))}
       </div>
       <Footer />
     </>
