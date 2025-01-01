35faa777e2cfd56d5c9ba529115725ae72990a29
Leanghok <leanghokoeng5@gmail.com>
2025-01-01 12:32:04 +0700

    Add a logout button and fetch user's posts

--- src/routes/profile/+page.server.ts
+++ src/routes/profile/+page.server.ts
@@ -1,10 +1,29 @@
-import { redirect } from '@sveltejs/kit';
+import { fail, redirect, type Actions } from '@sveltejs/kit';
 import type { PageServerLoad } from '../signup/$types';
+import * as auth from '$lib/server/auth';
+import { db } from '$lib/server/db';
+import { eq } from 'drizzle-orm';
+import { post } from '$lib/server/db/schema';
 
 export const load: PageServerLoad = async (event) => {
 	if (!event.locals.user) {
 		return redirect(301, '/login');
 	}
 
-	return { user: event.locals.user };
+	const posts = await db.query.post.findMany({ where: eq(post.userId, event.locals.user.id) });
+
+	return { user: event.locals.user, posts };
+};
+
+export const actions: Actions = {
+	logout: async (event) => {
+		if (!event.locals.session) {
+			return fail(401);
+		}
+
+		await auth.invalidateSession(event.locals.session.id);
+		auth.deleteSessionTokenCookie(event);
+
+		return redirect(301, '/login');
+	}
 };
--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -2,12 +2,29 @@
 	import * as Card from '$lib/components/ui/card';
 	import * as Avatar from '$lib/components/ui/avatar';
 	import type { PageData } from '../$types';
+	import { Button } from '$lib/components/ui/button';
+	import { LogOut } from 'lucide-svelte';
 
 	let { data }: PageData = $props();
+
+	function formatDate(dateString: string) {
+		const date = new Date(dateString);
+		return new Intl.DateTimeFormat('en-GB', {
+			day: '2-digit',
+			month: '2-digit',
+			year: 'numeric'
+		}).format(date);
+	}
 </script>
 
 <Card.Root class="mx-auto max-w-96">
-	<Card.Content>
+	<Card.Content class="relative">
+		<form action="?/logout" method="POST">
+			<Button variant="outline" size="icon" class="absolute right-5 top-5" type="submit">
+				<LogOut />
+				<span class="sr-only">Logout</span>
+			</Button>
+		</form>
 		<Avatar.Root class="size-20">
 			<Avatar.Image src="https://github.com/shadcn.png" alt="@shadcn" />
 			<Avatar.Fallback>CN</Avatar.Fallback>
@@ -17,18 +34,16 @@
 
 		<h2 class="mt-4 text-2xl font-bold">Posts</h2>
 		<ul class="mt-2 space-y-2">
-			<li class="flex items-center justify-between">
-				<a href="#" class="underline">Markdown</a>
-				<p>20/12/2024</p>
-			</li>
-			<li class="flex items-center justify-between">
-				<a href="#" class="underline">Switching from neovim to vscode</a>
-				<p>20/12/2024</p>
-			</li>
-			<li class="flex items-center justify-between">
-				<a href="#" class="underline">Learning sveltekit</a>
-				<p>20/12/2024</p>
-			</li>
+			{#if data.posts.length === 0}
+				<li>No posts found</li>
+			{/if}
+
+			{#each data.posts as post}
+				<li class="flex items-center justify-between">
+					<a href="/posts/{post.id}" class="underline">{post.title}</a>
+					<p>{formatDate(post.createdAt)}</p>
+				</li>
+			{/each}
 		</ul>
 	</Card.Content>
 </Card.Root>
