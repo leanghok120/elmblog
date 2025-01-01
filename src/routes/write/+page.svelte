60a7550a088d306d8837f741375c71299ec11309
Leanghok <leanghokoeng5@gmail.com>
2025-01-01 10:25:41 +0700

    Finish write page ui

---
+++ src/routes/write/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { Input } from '$lib/components/ui/input';
+	import { Textarea } from '$lib/components/ui/textarea';
+</script>
+
+<h1 class="text-4xl font-black">Write</h1>
+<Input placeholder="Title" class="mt-6" />
+<Textarea placeholder="Write here..." class="mt-4 h-52" />
