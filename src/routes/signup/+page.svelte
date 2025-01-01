055b97b0c6a885305dae52c0aab7da9b8d049594
Leanghok <leanghokoeng5@gmail.com>
2025-01-01 12:17:57 +0700

    Fix signup/login page

--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -9,29 +9,31 @@
 	let { form }: { form: ActionData } = $props();
 </script>
 
-<Card.Root class="w-full max-w-sm">
-	<Card.Header>
-		<Card.Title class="text-2xl">Signup</Card.Title>
-	</Card.Header>
-	<Card.Content>
-		<form action="?/register" method="POST" class="grid gap-4" use:enhance>
-			<div class="grid gap-2">
-				<Label for="username">Username</Label>
-				<Input id="username" name="username" type="text" placeholder="johndoe120" required />
-			</div>
-			<div class="grid gap-2">
-				<Label for="password">Password</Label>
-				<Input id="password" name="password" type="password" required />
-			</div>
-			<Button class="w-full" type="submit">Signup</Button>
-			{#if form?.message}
-				<p class="text-red-500">{form?.message}</p>
-			{/if}
-		</form>
-	</Card.Content>
-	<Card.Footer>
-		<p class="w-full text-center text-sm">
-			Already have an account? <a href="/login" class="underline">Login</a>
-		</p>
-	</Card.Footer>
-</Card.Root>
+<div class="flex items-center justify-center">
+	<Card.Root class="w-full max-w-sm">
+		<Card.Header>
+			<Card.Title class="text-2xl">Signup</Card.Title>
+		</Card.Header>
+		<Card.Content>
+			<form action="?/register" method="POST" class="grid gap-4" use:enhance>
+				<div class="grid gap-2">
+					<Label for="username">Username</Label>
+					<Input id="username" name="username" type="text" placeholder="johndoe120" required />
+				</div>
+				<div class="grid gap-2">
+					<Label for="password">Password</Label>
+					<Input id="password" name="password" type="password" required />
+				</div>
+				<Button class="w-full" type="submit">Signup</Button>
+				{#if form?.message}
+					<p class="text-red-500">{form?.message}</p>
+				{/if}
+			</form>
+		</Card.Content>
+		<Card.Footer>
+			<p class="w-full text-center text-sm">
+				Already have an account? <a href="/login" class="underline">Login</a>
+			</p>
+		</Card.Footer>
+	</Card.Root>
+</div>
