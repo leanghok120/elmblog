40f7bc57fb21f0f7e719a4ac30b66ad652b46c6b
Leanghok <leanghokoeng5@gmail.com>
2025-01-01 11:42:02 +0700

    Fetch only your own post in home page

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,11 +1,12 @@
 import { db } from '$lib/server/db';
-import { desc } from 'drizzle-orm';
+import { desc, eq } from 'drizzle-orm';
 import type { PageServerLoad } from './signup/$types';
 import { post } from '$lib/server/db/schema';
 
-export const load: PageServerLoad = async () => {
+export const load: PageServerLoad = async ({ locals }) => {
 	const posts = await db.query.post.findMany({
-		orderBy: [desc(post.createdAt)]
+		orderBy: [desc(post.createdAt)],
+		where: eq(post.userId, locals.user.id)
 	});
 
 	return { posts };
