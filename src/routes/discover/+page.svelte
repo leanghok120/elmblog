7e08e636fb519b87a79f7705250b8248e72bddde
Leanghok <leanghokoeng5@gmail.com>
2025-01-01 10:17:15 +0700

    Finish discover page ui

---
+++ src/routes/discover/+page.svelte
@@ -0,0 +1,12 @@
+<h1 class="text-4xl font-black">Discover</h1>
+
+<ul class="mt-5 space-y-2 text-lg">
+	<li class="flex items-center justify-between">
+		<a href="#" class="underline">2024 wrapped</a>
+		<p>31/12/2024</p>
+	</li>
+	<li class="flex items-center justify-between">
+		<a href="#" class="underline">My obsession with the artic monkeys</a>
+		<p>10/02/2024</p>
+	</li>
+</ul>
