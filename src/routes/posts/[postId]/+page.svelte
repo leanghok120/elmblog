55c5f7cc0f7a575c6e0733bdbe6f05825a76a4c7
Leanghok <leanghokoeng5@gmail.com>
2025-01-02 17:29:25 +0700

    add use:enhance to all forms

--- src/routes/posts/[postId]/+page.svelte
+++ src/routes/posts/[postId]/+page.svelte
@@ -2,6 +2,7 @@
 	import type { PageData } from './$types';
 	import { Button } from '$lib/components/ui/button';
 	import { Trash } from 'lucide-svelte';
+	import { enhance } from '$app/forms';
 
 	let { data }: PageData = $props();
 	let p = data.p;
@@ -25,7 +26,7 @@
 </div>
 <p class="mt-5">{p.content}</p>
 {#if user && user.id === p.userId}
-	<form action="?/deletePost" method="POST" class="mt-5">
+	<form action="?/deletePost" method="POST" class="mt-5" use:enhance>
 		<Button variant="outline" size="icon" type="submit">
 			<Trash />
 			<span class="sr-only">Delete post</span></Button
